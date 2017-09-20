d6a8afe81785efe7a3436e23410b523537710a5e
PC24 <BW-IT-PC24>
2017-09-20 10:06:39 +0200

    gfgfgh

--- Moebelverwaltung/Material.cs
+++ Moebelverwaltung/Material.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,8 +10,10 @@ namespace BBW.Moebelverwaltung
     // Aufzählungsdatentyp, der mehrere Konstanten gruppiert
     // Enumeration
     // Konstanten sind vom Datentyp int (als Wert automatisch int-Werte beginnend ab 0 zugewiesen)
+    // Konstantennamen müssen sich an Regeln für Bezeichner halten
     /// <summary>
-    /// Enum, das die Materialarten für Möbelstücke enthält
+    /// Enum, das die Materialarten für Möbelstücke enthält und als Eigenschaftstyp in Moebelstueck verwendet wird
     /// </summary>
     public enum Material{Holz,Stahl,Glas,Kunststoff}
+    
 }
