2aad282f2106a032fe073e756a289ed7ef1a2b22
PC24 <BW-IT-PC24>
2017-10-11 11:41:05 +0200

    if string.contains("braun") {   string.Wechseln(); }

--- MoebelverwaltungAnwendung/Program.cs
+++ MoebelverwaltungAnwendung/Program.cs
@@ -14,6 +14,54 @@ namespace MoebelverwaltungAnwendung
             //Klasse sowie deren Member müssen public sein, um sie in diesem Projekt verwenden zu können
             Tisch t1 = new Tisch();
             Console.WriteLine(t1.ToString());
+
+            List<Tisch> tische = new List<Tisch>();
+            tische.Add(new Tisch(100, 80, 90, "braun"));
+            tische.Add(new Tisch(150, 80, 90, "hellbraun"));
+            tische.Add(new Tisch(180, 80, 90, "rotbraun"));
+            tische.Add(new Tisch(110, 80, 90, "blau"));
+            tische.Add(new Tisch(200, 80, 90, "dunkelbraun"));
+            tische.Add(new Tisch(170, 80, 90, "beige"));
+            tische.Add(new Tisch(175, 100, 90, "grün"));
+
+            foreach (var item in tische)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            //Aufgabe: alle braunen Tische in einer separaten Liste speichern
+
+            //Lösungsvariante 1:
+            List<Tisch> listeBrauneTische = new List<Tisch>();
+            foreach (var item in tische)
+            {
+                string farbe = item.Farbe;
+                if (farbe.Contains("braun"))
+                {
+                    listeBrauneTische.Add(item);
+                }
+            }
+            Console.WriteLine("braune Tische: ");
+            for (int i = 0; i < listeBrauneTische.Count; i++)
+            {
+                Console.WriteLine(listeBrauneTische[i].ToString());
+            }
+
+            //Lösungsvariante 2:
+            //Verwendung von LINQ (Language Intergrated Query)
+            //Syntax:   from Bereichsvariable in Liste
+            //          where Bedingung(en)
+            //          select Bereichsvariable;
+            //Rückgabe ist ein Objekt vom Typ IEnumerable<Tisch>
+            //LINQ-Abfrage wird erst ausgeführt, wenn auf Ergebnismenge zugegriffen wird
+            //  (wenn Liste mittels foreach durchlaufen wird)
+            var listeBrauneTische2 = from tisch in tische
+                                     where tisch.Farbe.Contains("braun")
+                                     select tisch;
+            Console.WriteLine("Braune Tische -Variante2: ");
+            foreach (var item in listeBrauneTische2)
+            {
+                Console.WriteLine(item.ToString());
+            }
         }
     }
 }
