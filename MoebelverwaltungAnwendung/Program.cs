c2c2006adff9dc7422333390bd9062b179f608d0
PC24 <BW-IT-PC24>
2017-10-11 12:23:32 +0200

    trololololo

--- MoebelverwaltungAnwendung/Program.cs
+++ MoebelverwaltungAnwendung/Program.cs
@@ -54,14 +54,38 @@ namespace MoebelverwaltungAnwendung
             //Rückgabe ist ein Objekt vom Typ IEnumerable<Tisch>
             //LINQ-Abfrage wird erst ausgeführt, wenn auf Ergebnismenge zugegriffen wird
             //  (wenn Liste mittels foreach durchlaufen wird)
+            //Tische zusätzlich nach Länge sortiert
+            //Bereichsvariable tisch wird hier definiert und initialisiert
             var listeBrauneTische2 = from tisch in tische
                                      where tisch.Farbe.Contains("braun")
+                                     //absteigende Sortierung der Länge
+                                     orderby tisch.Laenge descending
                                      select tisch;
             Console.WriteLine("Braune Tische -Variante2: ");
             foreach (var item in listeBrauneTische2)
             {
                 Console.WriteLine(item.ToString());
             }
+
+            //Lösungsvariante 3:
+            //Erweiterungsmethodensyntax von LINQ
+            //Verwendung von Labda-Ausdrücken
+            //Syntax: MEthodenname((Parameternamen) => Parameter1.Eigenschaft == Value)
+            var listeBrauneTische3 = tische
+                                    .Where((tisch1) => tisch1.Farbe.Contains("braun")).OrderByDescending(tisch2 => tisch2.Laenge);
+            Console.WriteLine("Braune Tische -Variante3: ");
+            foreach (var item in listeBrauneTische3)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            //Anzahl der braunen Tische ermitteln (Abfrage-Syntax)
+            //Abfrage wird sofort ausgeführt
+            int anzahlBrauneTische = (from tisch in tische
+                                      where tisch.Farbe.Contains("braun")
+                                      select tisch).Count();
+            Console.WriteLine("Anzahl braune Tische: " + anzahlBrauneTische);
+
         }
     }
 }
